bf949a1b160d807ebf3a4bf73e5f63adee01f502
Javier Aquique <51524505+javieraquique@users.noreply.github.com>
2022-10-27 22:48:47 +0200

    Enabled streamlit app

--- app.py
+++ app.py
@@ -1,37 +1,68 @@
-import pandas as pd
-import requests
-import numpy as np
+from tkinter import Y
 import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import json
+from dotenv import load_dotenv
+import requests
+import plotly.express as px
 
-@st.cache
-def load_data(query='covid'):
-    url = "https://twitter154.p.rapidapi.com/search/search"
 
-    querystring = {"query":f"#{query}","section":"top","start_date":"2022-01-01"}
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
 
-    headers = {
-        "X-RapidAPI-Key": "c6a5a891ecmsh03d9ec66402dd34p18a3c6jsnbe65087e49e4",
-        "X-RapidAPI-Host": "twitter154.p.rapidapi.com"
-    }
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
 
-    res_dict = response.json()
-    res_dict = res_dict['results']
-    
-    df = pd.DataFrame.from_dict(res_dict)
-    return df
+load_dotenv()
+bearer_token = os.getenv("BEARER_TOKEN")
 
-st.title('Tweets of ICBMS')
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data()
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+search_url = "https://api.twitter.com/2/tweets/search/recent"
 
-st.subheader('Number of pickups by hour')
+query_params = {
+    'query': 'lang=en',
+    'place.fields': 'contained_within,country,country_code,full_name,geo,id,name,place_type',
+    'tweet.fields': 'author_id,created_at,geo,id,lang,possibly_sensitive,source,text,withheld',
+    'user.fields': 'id,location,name,protected,username,verified,withheld',
+    'max_results': 100}
 
-hist_values = np.histogram(
-    df['creation_date'].dt.hour, bins=24, range=(0,24))[0]
+def main():
 
+    st.title('The lastest 100 tweets in English')
+
+    # DATE_COLUMN = 'date/time'
+    # DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+    #             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+    @st.cache
+    def load_data():
+        json_response = connect_to_endpoint(search_url, query_params)
+        data = pd.DataFrame.from_dict(json_response['data'])
+        # lowercase = lambda x: str(x).lower()
+        # data.rename(lowercase, axis='columns', inplace=True)
+        # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
+
+    data_load_state = st.text('Loading data...')
+    data = load_data()
+    data_load_state.text("Data loaded and ready!")
+
+
+    st.subheader('Number of twitts by hour')
+    st.bar_chart(data['lang'])
+    st.dataframe(data)
+
+
+if __name__ == "__main__":
+    main()
